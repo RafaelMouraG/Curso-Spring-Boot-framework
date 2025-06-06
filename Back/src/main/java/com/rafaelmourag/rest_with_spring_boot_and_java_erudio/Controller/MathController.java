21865e91bf8a0d51830765d79f05f677245dfb69
Rafael Moura <rafaelganascinidemoura@gmail.com>
2025-06-05 22:32:55 -0300

    Adiciona controlador de operações matemáticas e implementa lógica no serviço MathService

--- Back/src/main/java/com/rafaelmourag/rest_with_spring_boot_and_java_erudio/Controller/MathController.java
+++ Back/src/main/java/com/rafaelmourag/rest_with_spring_boot_and_java_erudio/Controller/MathController.java
@@ -1,43 +1,69 @@
 package com.rafaelmourag.rest_with_spring_boot_and_java_erudio.Controller;
 
+import java.util.function.Function;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rafaelmourag.rest_with_spring_boot_and_java_erudio.Exception.UnsupportedMathOperationException;
+import com.rafaelmourag.rest_with_spring_boot_and_java_erudio.Service.MathService;
 
 @RestController
 @RequestMapping("/math")
 public class MathController {
-    // http://localhost:8080/math/sum/3/5
-    @RequestMapping("/sum/{numberOne}/{numberTwo}")
-    public Double sum (
-        @PathVariable("numberOne") String numberOne,
-         @PathVariable("numberTwo") String numberTWo
-    ) throws IllegalArgumentException {
-        if (!isNumeric(numberOne)|| !isNumeric(numberTWo))throw new UnsupportedMathOperationException("Please set a numeric value");
-        return convertToDouble(numberOne) + convertToDouble(numberTWo);
-    }
 
-    private Double convertToDouble(String strNumber) throws IllegalArgumentException{
-        if (strNumber == null|| strNumber.isEmpty()) throw new UnsupportedMathOperationException("Please set a numeric value");
-        String number = strNumber.replace(",", ".");
-        return Double.parseDouble(number);
-      
+
+  @Autowired
+  private MathService mathService;
+  // http://localhost:8080/math/sum/3/5
+  @RequestMapping("/sum/{numberOne}/{numberTwo}")
+  public Double sum(
+      @PathVariable("numberOne") String numberOne,
+      @PathVariable("numberTwo") String numberTwo) throws IllegalArgumentException {
+    return mathService.sum(numberOne, numberTwo);
+  }
+
+   @RequestMapping("subtraction/{numberOne}/{numberTwo}")
+  public double subtraction(
+      @PathVariable("numberOne") String numberOne,
+      @PathVariable("numberTwo") String numberTwo) throws IllegalArgumentException {
+    return mathService.subtraction(numberOne, numberTwo);
+
+  }
+
+  @RequestMapping("multi/{numberOne}/{numberTwo}")
+  public double multi(
+      @PathVariable("numberOne") String numberOne,
+      @PathVariable("numberTwo") String numberTwo) throws IllegalArgumentException {
+    
+        return mathService.multiplication(numberOne, numberTwo);
+  }
+
+  @RequestMapping("division/{numberOne}/{numberTwo}")
+  public double division(
+      @PathVariable("numberOne") String numberOne,
+      @PathVariable("numberTwo") String numberTwo) throws IllegalArgumentException {
+    
+        return mathService.division(numberOne, numberTwo);
+
+  }
+
+   @RequestMapping("average/{numberOne}/{numberTwo}")
+  public double average(
+      @PathVariable("numberOne") String numberOne,
+      @PathVariable("numberTwo") String numberTwo) throws IllegalArgumentException {
    
-        
-    }
+        return mathService.average(numberOne, numberTwo);
+  }
 
-    private boolean isNumeric(String strNumber) {
-      if (strNumber == null|| strNumber.isEmpty()) {
-        return false;
-      }
-      String number = strNumber.replace(",", ".");
-      return (number.matches("[-+]?[0-9]*\\.?[0-9]+"));
-      
-      
-    }
+   @RequestMapping("square-root/{number}")
+  public double squareRoot(
+      @PathVariable("number") String number) throws IllegalArgumentException {
+   
+        return mathService.squareRoot(number);
+  }
+  
 
-    // http://localhost:8080/math/subtraction/3/5
-    // http://localhost:8080/math/division/3/5
 }
