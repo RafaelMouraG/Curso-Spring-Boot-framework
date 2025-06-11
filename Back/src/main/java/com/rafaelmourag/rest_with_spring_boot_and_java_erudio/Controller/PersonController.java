edb00d31a3d6d88993020d6a6dcfcf98d43419bd
Rafael Moura <rafaelganascinidemoura@gmail.com>
2025-06-11 07:52:20 -0300

    Adiciona classe PersonController

---
+++ Back/src/main/java/com/rafaelmourag/rest_with_spring_boot_and_java_erudio/Controller/PersonController.java
@@ -0,0 +1,8 @@
+package com.rafaelmourag.rest_with_spring_boot_and_java_erudio.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+    
+}
