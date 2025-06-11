3dbff9f0fa1ef59115fc34b0c322d0dcc20563f2
Rafael Moura <rafaelganascinidemoura@gmail.com>
2025-06-11 08:15:14 -0300

    Adiciona injeção de dependência para PersonService na classe PersonController e adiciona logger na classe PersonService

--- Back/src/main/java/com/rafaelmourag/rest_with_spring_boot_and_java_erudio/Controller/PersonController.java
+++ Back/src/main/java/com/rafaelmourag/rest_with_spring_boot_and_java_erudio/Controller/PersonController.java
@@ -1,8 +1,14 @@
 package com.rafaelmourag.rest_with_spring_boot_and_java_erudio.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rafaelmourag.rest_with_spring_boot_and_java_erudio.Service.PersonService;
 
 @RestController
 public class PersonController {
     
+
+    @Autowired
+    private PersonService service;
 }
--- Back/src/main/java/com/rafaelmourag/rest_with_spring_boot_and_java_erudio/Service/PersonService.java
+++ Back/src/main/java/com/rafaelmourag/rest_with_spring_boot_and_java_erudio/Service/PersonService.java
@@ -1,6 +1,7 @@
 package com.rafaelmourag.rest_with_spring_boot_and_java_erudio.Service;
 
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
 
 import org.springframework.stereotype.Service;
 
@@ -8,4 +9,5 @@ import org.springframework.stereotype.Service;
 public class PersonService {
     
     private final AtomicLong counter = new AtomicLong();
+    private Logger logger = Logger.getLogger(PersonService.class.getName());
 }
